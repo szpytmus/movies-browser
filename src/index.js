5300d20504c0e860d48b6cf17a9abe3dc25e8de1
Marcin Szpytma <ma.marcinsz@gmail.com>
2021-02-03 13:42:32 +0100

    style-components, reduxjs/toolkit; theme, GStyles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import {GlobalStyle} from './GlobalStyle';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
